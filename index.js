6dadf6c685f66285eb0590fbc16ba208b08e7ed3
Brandon Bakus <brandonbakus@gmail.com>
2024-12-03 20:31:40 -0500

    This test really frustrated me. I made some code that I was really happy with, even using two loops in one function.Turned out it just wanted something beyond generic that was somehow even harder to figure out than something more complex. These questions need to be framed differently, outside the index.test file. Seriously.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+
+const gifters = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(gifters){
+  const messages = [];
+  for (let i = 0; i < gifters.length; i++){
+    messages.push(`Thank you, ${gifters[i]}, for the wonderful surprise gift!`)
+  }
+  return messages;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+
